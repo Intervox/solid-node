8622ea5185093f0d7f6ad506885fb87f9f952c59
Leonid Beschastny <lbeschastny@pliq.me>
2013-11-29 19:16:41 +0400

    Refactored amon to support node 0.10.x.

--- test/amon-test.js
+++ test/amon-test.js
@@ -19,10 +19,9 @@ exports.testExceptionHandler = function(test){
   var exception_label = "Exception here";
   try {
     throw new Error(exception_label);
-  } 
+  }
   catch(error) {
-    var doc = Amon.exception_data_to_json(error);
-    var json = JSON.parse(doc);
+    var json = Amon.exception_data(error);
 
     test.equal(json.message, exception_label);
     test.equal(json.exception_class, "Error: " + exception_label);
@@ -33,10 +32,9 @@ exports.testExceptionHandler = function(test){
 exports.testExceptionHandlerWithoutLabel = function(test){
   try {
     throw new Error;
-  } 
+  }
   catch(error) {
-    var doc = Amon.exception_data_to_json(error);
-    var json = JSON.parse(doc);
+    var json = Amon.exception_data(error);
 
     test.equal(json.message, "");
     test.equal(json.exception_class, "Error");
